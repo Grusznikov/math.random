209697a69b02aac61b26573c6f6885d76527953c
Szymon <33131463+mikikox123@users.noreply.github.com>
2019-03-05 16:43:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const one = document.querySelector(".one-number");
 const five = document.querySelector(".five-number")
-let counter = 0;
 const ul = document.querySelector("ul")
 const min = 1;
 const max = 100;
+let counter = 0;
 
 function getRandomArbitrary(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -11,16 +11,18 @@ function getRandomArbitrary(min, max) {
 
 one.addEventListener("click", function newOneLi()
 {
-    if(counter < 1){ 
+    if(counter < 1)
+{ 
     counter++
+    counter == 0;
     var li = document.createElement("li");
     ul.appendChild(li);
     li.appendChild(document.createTextNode(getRandomArbitrary(min, max)))
-    }
-})
+}})
 
 five.addEventListener("click", function() {
     if(counter < 1){ 
+        counter == 0;
     for(i=0; counter<5; counter++)
     {
         var li = document.createElement("li");
